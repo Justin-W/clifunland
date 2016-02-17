dbbc4db96a4f5a5cae784d33c2fc65c1b0001c36
Justin W <justin.webster@gmail.com>
2016-02-17 13:52:39 -0800

    json_utils: added a new module.

---
+++ src/clifunzone/json_utils.py
@@ -0,0 +1,47 @@
+import json
+
+
+def contains_valid_json(obj):
+    """
+    Indicates whether a specified value contains
+
+    :param obj:
+    :return:
+
+    >>> contains_valid_json(None)
+    False
+
+    >>> contains_valid_json('')
+    False
+
+    >>> contains_valid_json('{')
+    False
+
+    >>> contains_valid_json('{}')
+    True
+
+    >>> contains_valid_json('{"constants": {"pi": 3.14}}')
+    True
+    """
+    if obj is None:
+        return False
+    try:
+        try:
+            # json_object = json.loads(obj.read())
+            d = json.load(obj)
+        except AttributeError:
+            # obj is not a file
+            d = json.loads(obj)
+    except ValueError:
+        return False
+    return True
+
+
+def main():
+    import doctest
+    fail, total = doctest.testmod(optionflags=(doctest.REPORT_NDIFF | doctest.REPORT_ONLY_FIRST_FAILURE))
+    print 'Doctest: {f} FAILED ({p} of {t} PASSED).'.format(f=fail, p=(total - fail), t=total)
+
+
+if __name__ == "__main__":
+    main()
