164800dcf5eeade2ebba2958fdbbf93ad0ea670f
Justin W <justin.webster@gmail.com>
2016-02-17 13:52:27 -0800

    reflection_utils: added a new module.

---
+++ src/clifunzone/reflection_utils.py
@@ -0,0 +1,41 @@
+import sys
+
+
+def is_file_like(obj):
+    """
+    Indicates whether a specified value is a 'file-like' object.
+
+    :param obj: the object/value.
+    :return:
+
+    >>> is_file_like(None)
+    False
+
+    >>> is_file_like('')
+    False
+
+    >>> is_file_like('abc')
+    False
+
+    >>> is_file_like([])
+    False
+
+    >>> is_file_like({})
+    False
+
+    >>> is_file_like(sys.stdin)
+    True
+    """
+    if obj is None:
+        return False
+    return hasattr(obj, 'read')
+
+
+def main():
+    import doctest
+    fail, total = doctest.testmod(optionflags=(doctest.REPORT_NDIFF | doctest.REPORT_ONLY_FIRST_FAILURE))
+    print 'Doctest: {f} FAILED ({p} of {t} PASSED).'.format(f=fail, p=(total - fail), t=total)
+
+
+if __name__ == "__main__":
+    main()
