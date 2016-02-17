2918e8e2c0b53a97c94a5a47a74c39b9529e3f4c
Justin W <justin.webster@gmail.com>
2016-02-17 14:44:25 -0800

    xml_utils: added a new load() function to parse xml files/strings.

--- src/clifunzone/xml_utils.py
+++ src/clifunzone/xml_utils.py
@@ -37,6 +37,39 @@ def contains_valid_xml(obj):
     return True
 
 
+def load(obj):
+    """
+    Parses a specified object using ElementTree.
+
+    :param obj: a file-like or string-like object.
+    :return: True if valid, else False.
+
+    >>> load(None)
+    Traceback (most recent call last):
+    ValueError
+
+    >>> load('')
+    Traceback (most recent call last):
+    ParseError: no element found: line 1, column 0
+
+    >>> load('<')
+    Traceback (most recent call last):
+    ParseError: unclosed token: line 1, column 0
+
+    >>> load('<abc />').tag
+    'abc'
+
+    >>> load('<constants><constant id="pi" value="3.14" /><constant id="zero">0</constant></constants>').tag
+    'constants'
+    """
+    if obj is None:
+        raise ValueError
+    if reflection_utils.is_file_like(obj):
+        # read the contents of obj
+        obj = obj.read()
+    return ET.fromstring(obj)
+
+
 def main():
     import doctest
     fail, total = doctest.testmod(optionflags=(doctest.REPORT_NDIFF | doctest.REPORT_ONLY_FIRST_FAILURE))
