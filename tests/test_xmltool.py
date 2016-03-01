488e5669145cf4761f0699da7534f17206bfe559
Justin W <justin.webster@gmail.com>
2016-03-01 12:02:30 -0800

    xmltool: tests: elements command: added tests.

--- tests/test_xmltool.py
+++ tests/test_xmltool.py
@@ -224,3 +224,28 @@ def test_tojson(input_text, cli_args, expected):
 ])
 def test_tojson_invalid_input(input_text):
     invoke_sut_piped_input(sut.tojson, [], input_text, exit_code=-1, expected=None)
+
+
+@pytest.mark.parametrize("input_text,expected", [
+    ('<abc/>', '{"path":"/abc","content":{"tag":"abc"}}'),
+    ('<a>\t<b><c/> </b></a>', [
+        '{"path":"/a","content":{"#text":"\\t","tag":"a"},"metrics":{"children":{"count":1,"tags":["b"]},"descendants":{"count":2,"tags":["b","c"]}}}',
+        '{"path":"/a/b","content":{"tag":"b"},"metrics":{"children":{"count":1,"tags":["c"]},"descendants":{"count":1,"tags":["c"]}}}',
+        '{"path":"/a/b/c","content":{"tag":"c"}}'
+    ])
+])
+def test_elements(input_text, expected):
+    invoke_sut_piped_input(sut.elements, [], input_text, exit_code=0, expected=expected)
+
+
+@pytest.mark.parametrize("input_text", [
+    '',
+    ' ',
+    '<<<',
+    '>',
+    '<a>',
+    'abc',
+    '{"a": null}'
+])
+def test_elements_invalid_input(input_text):
+    invoke_sut_piped_input(sut.elements, [], input_text, exit_code=-1, expected=None)
