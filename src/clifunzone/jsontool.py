d236b4ecac2e87aeb67ed147aa824d0e75cb6b4f
Justin W <justin.webster@gmail.com>
2016-03-02 15:32:55 -0800

    jsontool: code cleanup and improvement (debugging and logging code).

--- src/clifunzone/jsontool.py
+++ src/clifunzone/jsontool.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import sys
 from pprint import pformat
 
@@ -11,7 +12,7 @@ from clifunzone.dict_utils import flatten
 from clifunzone.reflection_utils import varsdict
 
 
-def process(**kwargs):
+def debug_context(**kwargs):
     ctx = click.get_current_context()
 
     # debug_ = ctx.params.get('debug') or ctx.parent.params.get('debug') if ctx.parent else False
@@ -27,9 +28,8 @@ def process(**kwargs):
         click_utils.echo_context(ctx)
         click_utils.echo_kwargs(kwargs)
 
-    subcommand = ctx.invoked_subcommand
-    if subcommand == 'info':
-        pass
+        subcommand = ctx.invoked_subcommand
+        click.echo('subcommand: %s' % subcommand)
 
 
 @click.group(context_settings=click_utils.CONTEXT_SETTINGS, invoke_without_command=True)
@@ -51,7 +51,12 @@ def cli(debug):
         click.echo('I was invoked without a subcommand...')
     else:
         if debug:
-            click.echo('I am about to invoke subcommand: %s.' % subcommand)
+            click.echo('I was invoked with subcommand: %s.' % subcommand)
+
+    try:
+        debug_context(**{})
+    except:
+        logging.exception('debug_context error')
 
 
 @cli.command(short_help='echo the unparsed input')
