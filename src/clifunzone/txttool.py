94ba4b51b97667c3d4975a900961e5f8b0886aca
Justin W <justin.webster@gmail.com>
2016-03-04 09:18:06 -0800

    txttool: added a new tool for interacting with TXT files/data.

---
+++ src/clifunzone/txttool.py
@@ -0,0 +1,116 @@
+import json
+import logging
+from pprint import pformat
+
+import click
+
+from clifunzone import click_utils
+from clifunzone import dict_utils
+from clifunzone import txt_utils
+
+
+def debug_context(**kwargs):
+    ctx = click.get_current_context()
+
+    click_utils.inherit_parent_params(ctx, ('debug',))
+
+    debug_ = ctx.params['debug']
+
+    if debug_:
+        click.echo('Debug mode: %s' % ('enabled' if debug_ else 'disabled'))
+        click_utils.echo_context(ctx)
+        click_utils.echo_kwargs(kwargs)
+
+        subcommand = ctx.invoked_subcommand
+        click.echo('subcommand: %s' % subcommand)
+
+
+@click.group(context_settings=click_utils.CONTEXT_SETTINGS, invoke_without_command=True)
+@click.version_option(version='1.0.0')
+@click.option('--debug/--silent', '-d/-s', 'debug', default=False)
+# @click.option('--debug', '-d', 'debug', flag_value=True, default=True)
+# @click.option('--silent', '-s', 'debug', flag_value=False)
+def cli(debug):
+    """
+    Provides CLI commands for interacting with text data/files.
+    """
+    ctx = click.get_current_context()
+    if debug:
+        click_utils.echo_context(ctx)
+
+    subcommand = ctx.invoked_subcommand
+    if subcommand is None:
+        click.echo('I was invoked without a subcommand...')
+    else:
+        if debug:
+            click.echo('I was invoked with subcommand: %s.' % subcommand)
+
+    try:
+        debug_context(**{})
+    except:
+        logging.exception('debug_context error')
+
+
+@cli.command(short_help='echo the unparsed input')
+@click.option('--input', '-i', type=click.Path(exists=True, dir_okay=False, allow_dash=True),
+              help="the path to the file containing the input. Or '-' to use stdin (e.g. piped input).")
+def echo(input, **kwargs):
+    """
+    Echo the (unparsed) input.
+    """
+    if not input:
+        input = '-'
+    with click.open_file(input, mode='rb') as f:
+        s = f.read()
+        click.echo(s)
+
+
+@cli.command()
+@click.option('--input', '-i', type=click.Path(exists=True, dir_okay=False, allow_dash=True),
+              help="the path to the file containing the input. Or '-' to use stdin (e.g. piped input).")
+@click.option('--json', '-oj', 'output_format', flag_value='json',
+              help='renders the output as JSON.')
+@click.option('--python', '--dict', '-od', 'output_format', flag_value='dict',
+              help='renders the output as a python dictionary.')
+@click.option('--xml', '-ox', 'output_format', flag_value='xml',
+              help='renders the output as XML.')
+@click.option('--flatten', '--flat', '-f', 'flat', is_flag=True, type=click.BOOL,
+              help='enables a flattened (vs. nested) hierarchical structure for the output.')
+@click.option('--verbose', '-v', is_flag=True, type=click.BOOL,
+              help='enables more detailed output.')
+def info(input, output_format, verbose, flat, **kwargs):
+    """
+    Provides info about the input.
+    """
+    if not input:
+        input = '-'
+    with click.open_file(input, mode='rb') as f:
+        data = f.read()
+        d = {}
+        d.update({'content': txt_utils.get_info(data)})
+        # if verbose:
+        #     d['_object'] = {
+        #         'type': type(data),
+        #         'members': sorted(varsdict(data).keys())
+        #     }
+        if flat:
+            d = dict_utils.flatten(d)
+
+        if not output_format:
+            output_format = 'json'
+        if output_format == 'dict':
+            s = pformat(d)
+        elif output_format == 'json':
+            s = json.dumps(d, indent=2, sort_keys=True)
+        # elif output_format == 'xml':
+        else:
+            raise NotImplementedError('Unsupported output format: %s' % output_format)
+        click.echo(s)
+
+
+def main():
+    cli()
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_txttool.py
@@ -0,0 +1,194 @@
+import sys
+
+import pytest
+
+import clifunzone.txttool as sut
+from click_testing_utils import clirunner_invoke_piped
+
+
+def test_none():
+    expected = 'I was invoked without a subcommand...'
+    clirunner_invoke_piped(sut.cli, [], '', exit_code=0, out_ok=expected)
+
+
+def test_none_debug():
+    expected = [
+        '',
+        "ctx:",
+        "{'_close_callbacks': [],",
+        " '_depth': 2,",
+        " '_meta': {},",
+        " 'allow_extra_args': True,",
+        " 'allow_interspersed_args': False,",
+        " 'args': [],",
+        " 'auto_envvar_prefix': None,",
+        " 'color': None,",
+        " 'command': <click.core.Group object at 0x10c6d5ed0>,",
+        " 'default_map': None,",
+        " 'help_option_names': ['-h', '--help'],",
+        " 'ignore_unknown_options': False,",
+        " 'info_name': 'cli',",
+        " 'invoked_subcommand': None,",
+        " 'max_content_width': None,",
+        " 'obj': None,",
+        " 'params': {'debug': True},",
+        " 'parent': None,",
+        " 'protected_args': [],",
+        " 'resilient_parsing': False,",
+        " 'terminal_width': None,",
+        " 'token_normalize_func': None}",
+        '',
+        'I was invoked without a subcommand...',
+        'Debug mode: enabled',
+        '',
+        "ctx:",
+        "{'_close_callbacks': [],",
+        " '_depth': 2,",
+        " '_meta': {},",
+        " 'allow_extra_args': True,",
+        " 'allow_interspersed_args': False,",
+        " 'args': [],",
+        " 'auto_envvar_prefix': None,",
+        " 'color': None,",
+        " 'command': <click.core.Group object at 0x10c6d5ed0>,",
+        " 'default_map': None,",
+        " 'help_option_names': ['-h', '--help'],",
+        " 'ignore_unknown_options': False,",
+        " 'info_name': 'cli',",
+        " 'invoked_subcommand': None,",
+        " 'max_content_width': None,",
+        " 'obj': None,",
+        " 'params': {'debug': True},",
+        " 'parent': None,",
+        " 'protected_args': [],",
+        " 'resilient_parsing': False,",
+        " 'terminal_width': None,",
+        " 'token_normalize_func': None}",
+        '',
+        'kwargs: {}',
+        'subcommand: None'
+    ]
+    clirunner_invoke_piped(sut.cli, ['-d'], '', exit_code=0, out_ok=expected)
+
+
+@pytest.mark.parametrize("input_text", [
+    '<abc/>',
+    '{"abc": null}',
+    'abc',
+    'Hi. How are you? My name is John. What is your name?'
+])
+def test_echo(input_text):
+    expected = input_text or ['']
+    clirunner_invoke_piped(sut.echo, [], input_text, exit_code=0, out_ok=expected)
+
+
+@pytest.mark.parametrize("input_text,cli_args,expected", [
+    ('abc', ['-oj'], [
+        '{',
+        '  "content": {',
+        '    "length": 3,',
+        '    "metrics": {',
+        '      "chars": {',
+        '        "counts": {',
+        '          "distinct": 3,',
+        '          "each": {',
+        '            "a": 1,',
+        '            "b": 1,',
+        '            "c": 1',
+        '          },',
+        '          "total": 3',
+        '        }',
+        '      },',
+        '      "words": {',
+        '        "counts": {',
+        '          "distinct": 1,',
+        '          "each": {',
+        '            "abc": 1',
+        '          },',
+        '          "total": 1',
+        '        }',
+        '      }',
+        '    }',
+        '  }',
+        '}'
+    ]),
+    ('<abc/>', ['-od'], [
+        "{'content': OrderedDict([('length', 6), ('metrics', {" +
+        "'chars': {'counts': {'distinct': 6, 'total': 6, " +
+        "'each': OrderedDict([('/', 1), ('<', 1), ('>', 1), ('a', 1), ('b', 1), ('c', 1)])}}, " +
+        "'words': {'counts': {'distinct': 2, 'total': 4, 'each': OrderedDict([('', 3), ('abc', 1)])}}})])}"
+    ]),
+    ('{"abc": null}', ['-od'], [
+     "{\'content\': OrderedDict([(\'length\', 13), (\'metrics\', {\'chars\': {" +
+     "\'counts\': {\'distinct\': 11, \'total\': 13, " +
+     "\'each\': OrderedDict([(\' \', 1), (\'\"\', 2), (\':\', 1), (\'a\', 1), (\'b\', 1), (\'c\', 1), (\'l\', 2), " +
+     "(\'n\', 1), (\'u\', 1), (\'{\', 1), (\'}\', 1)])}}, " +
+     "\'words\': {\'counts\': {\'distinct\': 3, \'total\': 7, " +
+     "\'each\': OrderedDict([(\'\', 5), (\'abc\', 1), (\'null\', 1)])}}})])}"
+    ]),
+    ('{"abc": null}', ['-f'], [
+        '{',
+        '  "content_length": 13,',
+        '  "content_metrics_chars_counts_distinct": 11,',
+        '  "content_metrics_chars_counts_each_ ": 1,',
+        '  "content_metrics_chars_counts_each_\\"": 2,',
+        '  "content_metrics_chars_counts_each_:": 1,',
+        '  "content_metrics_chars_counts_each_a": 1,',
+        '  "content_metrics_chars_counts_each_b": 1,',
+        '  "content_metrics_chars_counts_each_c": 1,',
+        '  "content_metrics_chars_counts_each_l": 2,',
+        '  "content_metrics_chars_counts_each_n": 1,',
+        '  "content_metrics_chars_counts_each_u": 1,',
+        '  "content_metrics_chars_counts_each_{": 1,',
+        '  "content_metrics_chars_counts_each_}": 1,',
+        '  "content_metrics_chars_counts_total": 13,',
+        '  "content_metrics_words_counts_distinct": 3,',
+        '  "content_metrics_words_counts_each_": 5,',
+        '  "content_metrics_words_counts_each_abc": 1,',
+        '  "content_metrics_words_counts_each_null": 1,',
+        '  "content_metrics_words_counts_total": 7',
+        '}'
+    ]),
+])
+@pytest.mark.skipif(sys.version_info > (3, 3),
+                    reason="currently broken for py35")
+def test_info(input_text, cli_args, expected):
+    clirunner_invoke_piped(sut.info, cli_args, input_text, exit_code=0, out_ok=expected)
+
+
+@pytest.mark.parametrize("input_text,cli_args,expected", [
+    ('Hi! How are you? My name is John Paul. What is your name?', ['-f'], [
+        '"content_length": 57,',
+        '"content_metrics_chars_counts_distinct": 20,',
+        '"content_metrics_chars_counts_each_ ": 12,',
+        '"content_metrics_chars_counts_each_y": 3,',
+        '"content_metrics_chars_counts_total": 57,',
+        '"content_metrics_words_counts_distinct": 12,',
+        '"content_metrics_words_counts_each_": 4,',
+        '"content_metrics_words_counts_each_is": 2,',
+        '"content_metrics_words_counts_each_john": 1,',
+        '"content_metrics_words_counts_each_name": 2,',
+        '"content_metrics_words_counts_each_paul": 1,',
+        '"content_metrics_words_counts_each_what": 1,',
+        '"content_metrics_words_counts_total": 17'
+    ]),
+    ("Hi! How are you? My name is John-Paul. What's your name?", ['-f'], [
+        '"content_length": 56,',
+        '"content_metrics_chars_counts_distinct": 22,',
+        '"content_metrics_chars_counts_each_ ": 10,',
+        '"content_metrics_chars_counts_each_!": 1,',
+        '"content_metrics_chars_counts_each_\'": 1,',
+        '"content_metrics_chars_counts_total": 56,',
+        '"content_metrics_words_counts_distinct": 11,',
+        '"content_metrics_words_counts_each_": 4,',
+        '"content_metrics_words_counts_each_is": 1,',
+        '"content_metrics_words_counts_each_john-paul": 1,',
+        '"content_metrics_words_counts_each_name": 2,',
+        '"content_metrics_words_counts_each_what\'s": 1,',
+        '"content_metrics_words_counts_total": 15'
+    ]),
+])
+@pytest.mark.skipif(sys.version_info > (3, 3),
+                    reason="currently broken for py35")
+def test_info_fragments(input_text, cli_args, expected):
+    clirunner_invoke_piped(sut.info, cli_args, input_text, exit_code=0, out_contains_seq=expected)
