07c8b5509258a7933bb86ff8cb306ff141c29355
Justin W <justin.webster@gmail.com>
2016-03-08 14:22:50 -0800

    setup, dependencies: modified setup.py to use the project-level requirements.txt to set the package's 'install_requires' settings.

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import io
+import itertools
+import os
 import re
 from glob import glob
 from os.path import basename
@@ -20,6 +22,57 @@ def read(*names, **kwargs):
         join(dirname(__file__), *names),
         encoding=kwargs.get('encoding', 'utf8')
     ).read()
+
+
+def load_requirements_file(fp=None, **kwargs):
+    """
+    Returns a list of the dependencies specified in a requirements.txt file.
+
+    Adapted from: http://stackoverflow.com/a/15341042
+
+    :param fp: the path of the requirements.txt file to load.
+    :param kwargs:
+    :return: a list of strings.
+
+    Usage Examples:
+
+    setup(
+        .....
+        install_requires=load_requirements_file('requirements.txt')
+    )
+
+    setup(
+        .....
+        install_requires=['some_package'] + load_requirements_file('requirements.txt')
+    )
+    """
+    if not fp:
+        fp = 'requirements.txt'
+    with open(fp) as f:
+        # values = f.read().splitlines()
+        values = [line for line in f.readlines()]
+    # strip the values
+    values = [i.strip() for i in values]
+    # filter out comment lines
+    values = [i for i in values if not i.startswith('#')]
+    # evaluate/load inherited/hierarchical req files properly (e.g. lines like '-r req.txt')
+    parent_files = [i[2:].strip() for i in values if i.startswith('-r ')]
+    if len(parent_files):
+        parent_files = [i[2:].strip() for i in values if i.startswith('-r ')]
+        # evaluate each parent_file path relative to the folder of the current file (fp)
+        cur_file = os.path.abspath(fp)
+        cur_dir = os.path.split(cur_file)[0]
+        parent_files = [os.path.join(cur_dir, f) for f in parent_files]
+        # load (recursively) the reqs from each parent_file
+        parent_values = itertools.chain([load_requirements_file(f) for f in parent_files])
+        # merge the inherited reqs (from the parent_files) with the non-inherited ones
+        values = [i for i in values if not i.startswith('-r ')]
+        values = itertools.chain(values, parent_values)
+    # filter out empty values
+    values = [i for i in values if len(i)]
+    # convert to a list instance
+    values = list(values)
+    return values
 
 
 setup(
@@ -64,9 +117,7 @@ setup(
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    install_requires=[
-        'click',
-    ],
+    install_requires=load_requirements_file('requirements.txt'),
     extras_require={
         # eg:
         #   'rst': ['docutils>=0.11'],
